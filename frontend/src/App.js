3c61a86ad4cdcf5fa8dba475268e64e40d80c293
Minjun1Kim <minjun3759@gmail.com>
2023-06-10 13:01:58 -0400

    Added CreatePost.js and configured routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import CreatePost from './pages/CreatePost'
+
 import { Routes, Route } from "react-router-dom"
 
 
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={ <Login/> } />
         <Route path="/signup" element={ <Signup/> } />
+        <Route path="/createpost" element={ <CreatePost/> } />
       </Routes>
     </div>
   )
