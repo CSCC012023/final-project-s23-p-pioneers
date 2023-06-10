f5be064aa5fd6bc7e9ac859cba93cf85a5525ddd
Vikram Narra <vchandra.narra@gmail.com>
2023-06-10 12:30:43 -0400

    Build: Setting up a router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,93 +1,18 @@
 import './App.css';
-import {useState} from 'react';
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import { Routes, Route } from "react-router-dom"
+
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const user = {
-      username,
-      email,
-      password,
-    };
-
-    fetch('http://localhost:8000/signup', { // Replace with your server URL
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Sign-up request failed.');
-        }
-      })
-      .then((data) => {
-        console.log(data); // handle the response from the server
-        setUsername('');
-        setEmail('');
-        setPassword('');
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
-
   return (
     <div>
-      <h2>Sign Up</h2>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsernameChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-          required
-        />
-      </div>
-      <button onClick={handleSubmit}>Sign Up</button>
+      <Routes>
+        <Route path="/login" element={ <Login/> } />
+        <Route path="/signup" element={ <Signup/> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
