c5c1308cf210082cb79c3c41d6eb41c80abf87aa
Minjun1Kim <minjun3759@gmail.com>
2023-06-16 19:07:59 -0400

    <feat> Frontend JobPosting.js

--- frontend/src/pages/JobPosting.js
+++ frontend/src/pages/JobPosting.js
@@ -1,15 +1,71 @@
 import React, { useState } from 'react';
 import { RxOpenInNewWindow } from 'react-icons/rx';
+import { Container, Grid, Box, Typography, alpha, Button, TextField, IconButton } from '@mui/material';
+import { CloudUpload } from '@mui/icons-material';
 import '../JobPosting.css'; // Import custom CSS file for styling
-// import girlImage from 'https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279';
 
+
+const jobsData = [
+
+  
+  {
+    title: 'Software Engineer',
+    company: 'Amazon',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://signsalad.com/wp-content/uploads/2021/11/Screenshot-2021-11-03-at-12.14.11.png',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Amazon',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://thumbs.dreamstime.com/b/amazon-logo-editorial-vector-illustration-market-136495269.jpg',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Apple',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://img.freepik.com/free-icon/mac-os_318-10374.jpg'
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Netflix',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Google',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://www.googlewatchblog.de/wp-content/uploads/google-logo-perfekt.jpg',
+  },
+
+  {
+    title: 'Software Engineer',
+    company: 'Nokia',
+    location: 'San Francisco',
+    type: 'Full-time',
+    image: 'https://www.nokia.com/sites/default/files/styles/scale_1440_no_crop/public/2023-02/nokia-refreshed-logo-2_1.png',
+  },
+
+  // Add more job objects here...
+];
 
 function JobPosting() {
-  const [title, setTitle] = useState('');
-  const [location, setLocation] = useState('');
-  const [jobDescription, setDescription] = useState('');
-  const [deadline, setDeadline] = useState('');
-  const [datePosted, setDate] = useState('');
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [title, setTitle] = React.useState('');
+  const [location, setLocation] = React.useState('');
+  const [jobDescription, setJobDescription] = React.useState('');
+  const [deadline, setDeadline] = React.useState('');
+  const [datePosted, setDatePosted] = React.useState('');
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -20,7 +76,7 @@ function JobPosting() {
   };
 
   const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
+    setJobDescription(event.target.value);
   };
 
   const handleDeadlineChange = (event) => {
@@ -28,7 +84,11 @@ function JobPosting() {
   };
 
   const handleDateChange = (event) => {
-    setDate(event.target.value);
+    setDatePosted(event.target.value);
+  };
+
+  const handleCreateAssessments = () => {
+    // Handle create assessments logic here
   };
 
   const handleSubmit = (event) => {
@@ -60,9 +120,9 @@ function JobPosting() {
         console.log(data); // handle the response from the server
         setTitle('');
         setLocation('');
-        setDescription('');
+        setJobDescription('');
         setDeadline('');
-        setDate('');
+        setDatePosted('');
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -70,156 +130,318 @@ function JobPosting() {
   };
 
   return (
-    <div>
-      <div className="job-posting-container">
-        <h2>Create Post</h2>
-        <form className="job-form">
-          <div className="form-group">
-            <label htmlFor="title">Job Title:</label>
-            <input
-              type="text"
-              id="title"
+    <Container maxWidth="md">
+    <Box textAlign="center" marginBottom={2}>
+        <Typography variant="h2" sx={{ color: 'white', fontFamily: 'Work-Sans' }}>
+          Create Post
+        </Typography>
+    </Box>
+
+      <form className="job-form" style={{ background: 'transparent' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Job Title"
+              variant="outlined"
               value={title}
               onChange={handleTitleChange}
               required
+              fullWidth
+              style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: '16px 20px',
+                gap: '12px',
+                width: '804px',
+                height: '46px',
+                background: '#FFFFFF',
+                border: '1px solid #858584',
+                borderRadius: '20px',
+              }}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="location">Job Location:</label>
-            <input
-              type="text"
-              id="location"
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Job Location"
+              variant="outlined"
               value={location}
               onChange={handleLocationChange}
               required
+              fullWidth
+              style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: '16px 20px',
+                gap: '12px',
+                width: '804px',
+                height: '46px',
+                background: '#FFFFFF',
+                border: '1px solid #858584',
+                borderRadius: '20px',
+              }}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Job Description:</label>
-            <textarea
-              id="description"
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Job Description"
+              variant="outlined"
+              multiline
+              rows={4}
               value={jobDescription}
               onChange={handleDescriptionChange}
               required
+              fullWidth
+              style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '16px 20px',
+                gap: '12px',
+                width: '804px',
+                height: '576px',
+                background: '#FFFFFF',
+                border: '1px solid #858584',
+                borderRadius: '20px',
+              }}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="deadline">Deadline:</label>
-            <input
-              type="text"
-              id="deadline"
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Deadline"
+              variant="outlined"
               value={deadline}
               onChange={handleDeadlineChange}
               required
+              fullWidth
+              style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: '16px 20px',
+                gap: '12px',
+                width: '804px',
+                height: '46px',
+                background: '#FFFFFF',
+                border: '1px solid #858584',
+                borderRadius: '20px',
+              }}
             />
-          </div>
-          <div className="form-group">
-            <label htmlFor="date">Date:</label>
-            <input
-              type="text"
-              id="date"
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Date"
+              variant="outlined"
               value={datePosted}
               onChange={handleDateChange}
               required
+              fullWidth
+              style={{
+                boxSizing: 'border-box',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: '16px 20px',
+                gap: '12px',
+                width: '804px',
+                height: '46px',
+                background: '#FFFFFF',
+                border: '1px solid #858584',
+                borderRadius: '20px',
+              }}
             />
-          </div>
-          <button className="submit-button" onClick={handleSubmit}>
-            Create Job
-          </button>
-        </form>
+          </Grid>
+
+          <Grid item xs={12}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item>
+              <input
+                accept="*"
+                style={{ display: 'none' }}
+                id="file-upload"
+                type="file"
+                onChange={handleFileChange}
+              />
+              <label htmlFor="file-upload" style={{ display: 'flex', alignItems: 'center' }}>
+                {/* <IconButton color="primary" component="span">
+                  <CloudUpload />
+                </IconButton> */}
+                <TextField
+                  label="Upload Files"
+                  variant="outlined"
+                  value={selectedFile ? selectedFile.name : ''}
+                  fullWidth
+                  disabled
+                  style={{
+                    boxSizing: 'border-box',
+                    width: '804px',
+                    height: '160px',
+                    background: '#F3F0FF',
+                    border: '1px dashed #C1B2FA',
+                    borderRadius: '8px',
+                  }}
+                />
+              </label>
+            </Grid>
+          </Grid>
+
+          </Grid>
+        </Grid>
+      </form>
+      
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', gap: '20px' }}>
+      <div className="button-container">
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '0px 50px',
+            gap: '12px',
+            width: '440px',
+            height: '60px',
+            background: '#A259FF',
+            borderRadius: '20px',
+          }}
+          onClick={handleCreateAssessments}
+        >
+          Create Optional Assessments
+        </Button>
       </div>
 
-      <div id="more">More from this company</div>
-
-      <div className="projects">
-        <div className="projects-container">
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>Meta</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <RxOpenInNewWindow />
-              </button>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>Google</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <RxOpenInNewWindow />
-              </button>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>Google</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <RxOpenInNewWindow />
-              </button>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>FaceBook</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <RxOpenInNewWindow />
-              </button>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>Microsoft</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <RxOpenInNewWindow />
-              </button>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src="https://cdn.shopify.com/s/files/1/0578/3696/1997/t/9/assets/lofiboy.gif?v=103461765217895835051680702279" alt="" />
-            <div className="portfolio-layer">
-              <h3>Google</h3>
-              <p></p>
-              <button
-                onClick={() => window.open('https://example.com')}
-                rel="noopener noreferrer"
-              >
-                <i className="bx bx-link-external"></i>
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className="button-container">
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '0px 50px',
+            gap: '12px',
+            width: '235px',
+            height: '60px',
+            background: '#A259FF',
+            borderRadius: '20px',
+          }}
+          onClick={handleSubmit}
+        >
+          Submit Post
+        </Button>
       </div>
     </div>
+    
+   
+    
+
+      <div className="more-company-container">
+        <div className="more-company-text">More from this company</div>
+        <button className="go-to-company-button">
+          <RxOpenInNewWindow className="button-icon" />
+          Go to company page
+        </button>
+      </div>
+      
+      
+    <div className="job-grid-container">
+      <JobGrid jobs={jobsData} />
+    </div>
+    </Container>
+    
   );
 }
+
+
+
+const JobGrid = ({ jobs }) => {
+  return (
+    <Container maxWidth="lg" sx={{ marginTop: '20px' }}> {/* Adjust the maxWidth value as per your design */}
+    <Grid container spacing={2}>
+      {jobs.length === 0 ? (
+        <Grid item xs={12}>
+          <Typography>No job postings from this company</Typography>
+        </Grid>
+      ) : (
+        jobs.map((job, index) => (
+          <Grid key={index} item xs={12} sm={6} md={4}>
+            <Box
+              sx={{
+                position: 'relative',
+                padding: 2,
+                border: '1px solid #e0e0e0',
+                borderRadius: 4,
+                transition: 'box-shadow 0.3s',
+                '&:hover': {
+                  boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.2)',
+                },
+                '&:hover::before': {
+                  content: '""',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: alpha('#A259FF', 0.2),
+                  zIndex: 1,
+                },
+              }}
+            >
+              <img
+                src={job.image}
+                alt={job.title}
+                style={{
+                  width: '100%',
+                  height: '295px',
+                  objectFit: 'cover',
+                  borderRadius: '4px',
+                }}
+              />
+              
+              <Typography
+                variant="subtitle1"
+                sx={{
+                  
+                  color: 'white',
+                  fontFamily: 'Work Sans',
+                  fontStyle: 'Sans Serif',
+                  fontWeight: 600,
+                  fontSize: '22px',
+                  lineHeight: '140%',
+                 
+                }}
+              >
+                {job.title}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                sx={{
+                  fontFamily: 'Work Sans, sans-serif',
+                  color: 'white',
+                }}
+              >
+                {`${job.company} • ${job.location} • ${job.type}`}
+              </Typography>
+            </Box>
+          </Grid>
+        ))
+      )}
+    </Grid>
+    </Container>
+  );
+};
+
+
+
 
 export default JobPosting;
