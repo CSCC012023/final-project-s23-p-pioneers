f0bd05e16f16071afbb689994f980fc6b935e189
AshwinM1523 <ashwinmallik@hotmail.com>
2023-06-17 22:33:42 -0400

    feat: implement cookie authentication
    This commit adds cookie authentication functionality to the website, allowing users to authenticate using cookies for a seamless login experience.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,45 @@
-import './App.css';
-import JobPosting from './pages/JobPosting'
-import Jobs from './pages/Jobs'
-import { Routes, Route } from "react-router-dom"
+import "./App.css";
+import JobPosting from "./pages/JobPosting";
+import Jobs from "./pages/Jobs";
+import { Routes, Route } from "react-router-dom";
 
 import { RequireAuth } from "react-auth-kit";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import CreatePost from './pages/CreatePost'
+import CreatePost from "./pages/CreatePost";
+import { Avatar, Typography, BottomNavigation } from "@mui/material";
+import Logo from "./assets/images/CoBuildLogo.png";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path="/login" element={ <Login/> } />
-        <Route path="/signup" element={ <Signup/> } />
-        <Route path="/jobs" element={ <Jobs />} />
-        <Route path="/jobpost/:id" element={ <JobPosting/> } />
-        <Route path="/createpost" element={ <CreatePost/> } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/jobs"
+          element={
+            <RequireAuth loginPath="/login">
+              <Jobs />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/jobpost/:id"
+          element={
+            <RequireAuth loginPath="/login">
+              <JobPosting />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/createpost"
+          element={
+            <RequireAuth loginPath="/login">
+              <CreatePost />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </div>
   );
