0726fce720ec6503ab412bd59a98738c25d226fb
andrewaucie <aucieandrew@gmail.com>
2023-07-06 23:44:38 -0400

    <feat>: integrating s3 storage

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const getPost = require('./API/getpost')
 const createPost = require('./API/createPost')
 const userLogin = require("./API/loginUser");
 const signUpRequest = require("./API/signUp.js");
+const generateUploadURL = require("./s3.js");
 
 app.use(cors());
 app.use(express.json()); // Add this line to parse JSON payloads
@@ -27,7 +28,18 @@ connect();
 app.listen(8000, () => {
   console.log("Server started on port 8000");
 });
+app.get('/s3Url', async (req, res) => {
+  console.log("heeere")
 
+  try {
+    const url = await generateUploadURL(req.query.username, req.query.type);
+    res.send({ url });
+    console.log(url);
+  } catch (error) {
+    console.error('Error generating upload URL:', error);
+    res.status(500).send('Error generating upload URL');
+  }
+});
 
 app.post('/createassesment', assesmentApi)
 app.post("/createpost", createPost);
