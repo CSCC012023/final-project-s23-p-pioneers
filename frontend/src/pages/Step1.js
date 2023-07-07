9a73ad2dbe6cd0fb7b1c9544d679b219a00f1a21
Minjun1Kim <minjun3759@gmail.com>
2023-07-07 16:07:53 -0400

    <feat>: Adding Educational Background

--- frontend/src/pages/Step1.js
+++ frontend/src/pages/Step1.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import {
   Typography,
   Box,
@@ -7,15 +7,28 @@ import {
   Checkbox,
   FormControlLabel,
   LinearProgress,
+  TextField,
+  Autocomplete,
+  Chip,
 } from "@mui/material";
 import { styled } from "@mui/system";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
+import Step4 from "./Step4";
 
 const languages = [
-  { name: 'Java', image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png' },
-  { name: 'C', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1200px-C_Programming_Language.svg.png' },
-  { name: 'Python', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png' },
+  { label: "Java", image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png' },
+  { label: "Python", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png' },
+  { label: "C", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1200px-C_Programming_Language.svg.png' },
+  { label: "C++", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png' },
+  { label: "JavaScript", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png' },
+  { label: "PHP", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png' },
+  { label: "SQL", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sql_data_base_with_logo.png/1200px-Sql_data_base_with_logo.png' },
+  { label: "Ruby", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1200px-Ruby_logo.svg.png' },
+  { label: "Swift", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/1200px-Swift_logo.svg.png' },
+  { label: "Go", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png' },
+  { label: "R", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/1200px-R_logo.svg.png' },
+  { label: "MATLAB", image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Matlab_Logo.png/1200px-Matlab_Logo.png' },
   // Add more languages with their respective images
 ];
 
@@ -26,12 +39,11 @@ const RootContainer = styled("div")({
   alignItems: "flex-start",
   justifyContent: "top",
   marginTop: "5%",
-  // marginLeft: "20%", // Adjust the left margin as needed
 });
 
 const Container = styled("div")({
   width: "40%",
-  marginLeft: "10%"
+  marginLeft: "10%",
 });
 
 const LanguageBox = styled("div")(({ theme }) => ({
@@ -50,27 +62,81 @@ const ContinueButton = styled(Button)(({ theme }) => ({
   marginTop: theme.spacing(2),
 }));
 
-const Step1 = () => {
+const Step1 = ({ handleNext }) => {
   const [selectedLanguages, setSelectedLanguages] = useState([]);
   const [activeStep, setActiveStep] = useState(1);
-  const totalSteps = 3;
+  const totalSteps = 4;
 
   const handleLanguageToggle = (language) => () => {
-    const selectedIndex = selectedLanguages.indexOf(language);
-    let newSelectedLanguages = [...selectedLanguages];
-
-    if (selectedIndex === -1) {
-      newSelectedLanguages = [...selectedLanguages, language];
-    } else {
-      newSelectedLanguages.splice(selectedIndex, 1);
-    }
-
+    const newSelectedLanguages = selectedLanguages.filter(
+      (selectedLanguage) => selectedLanguage.label !== language.label
+    );
     setSelectedLanguages(newSelectedLanguages);
   };
 
   const handleContinue = () => {
     setActiveStep((prevStep) => prevStep + 1);
   };
+
+  const handlePrevious = () => {
+    setActiveStep((prevStep) => prevStep - 1);
+  };
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+
+    if (event.key === "Enter" && value.trim() !== "") {
+      const existingLanguage = languages.find(
+        (language) => language.label.toLowerCase() === value.trim().toLowerCase()
+      );
+
+      if (!existingLanguage) {
+        const newLanguage = {
+          label: value.trim(),
+          image: "", // Set a default image for new languages
+        };
+        setSelectedLanguages((prevSelectedLanguages) => [
+          ...prevSelectedLanguages,
+          newLanguage,
+        ]);
+      }
+
+      event.target.value = "";
+    }
+  };
+
+  const handleLanguageChange = (_, value) => {
+    setSelectedLanguages(value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const value = event.target.value.trim();
+      if (value !== "") {
+        const newLanguage = {
+          label: value,
+          image: "", // Set a default image for new languages
+        };
+        if (!selectedLanguages.some((language) => language.label === newLanguage.label)) {
+          setSelectedLanguages((prevSelectedLanguages) => [
+            ...prevSelectedLanguages,
+            newLanguage,
+          ]);
+        }
+        event.target.value = "";
+      }
+    }
+  };
+
+  const renderOption = (props, option, state) => (
+    <li {...props} className={state.selected ? 'selected' : ''}>
+      <Box display="flex" alignItems="center">
+        <LanguageImage src={option.image} alt={option.label} />
+        <Typography variant="body1">{option.label}</Typography>
+      </Box>
+    </li>
+  );
 
   const renderStepContent = () => {
     switch (activeStep) {
@@ -80,21 +146,47 @@ const Step1 = () => {
             <Typography variant="h4" gutterBottom>
               What tools/languages do you use?
             </Typography>
-            {languages.map((language) => (
-              <LanguageBox key={language.name}>
-                <LanguageImage src={language.image} alt={language.name} />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={selectedLanguages.includes(language)}
-                      onChange={handleLanguageToggle(language)}
-                      color="primary"
-                    />
-                  }
-                  label={language.name}
+            <Autocomplete
+              multiple
+              freeSolo
+              options={languages}
+              value={selectedLanguages}
+              onChange={handleLanguageChange}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Languages"
+                  fullWidth
+                  onKeyDown={handleKeyDown}
                 />
-              </LanguageBox>
-            ))}
+              )}
+              renderOption={renderOption}
+              renderTags={(value, getTagProps) =>
+                value.map((language, index) => (
+                  <Chip
+                    key={language.label}
+                    label={language.label}
+                    onDelete={() => handleLanguageToggle(language)}
+                    {...getTagProps({ index })}
+                  />
+                ))
+              }
+              classes={{
+                paper: 'dropdown-menu',
+              }}
+            />
+            <style>{`
+              .dropdown-menu {
+                background-color: grey;
+              }
+              .dropdown-menu li {
+                padding: 8px;
+              }
+              .dropdown-menu li.selected {
+                background-color: lightgrey;
+              }
+            `}</style>
+
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <ContinueButton
@@ -115,9 +207,11 @@ const Step1 = () => {
           </div>
         );
       case 2:
-        return <Step2 handleNext={handleContinue} />;
+        return <Step2 handleNext={handleContinue} handlePrevious={handlePrevious} />;
       case 3:
-        return <Step3 handleNext={handleContinue}/>;
+        return <Step3 handleNext={handleContinue} handlePrevious={handlePrevious}/>;
+      case 4:
+        return <Step4 handleNext={handleContinue} handlePrevious={handlePrevious}/>;
       default:
         return null;
     }
--- frontend/src/pages/Step3.js
+++ frontend/src/pages/Step3.js
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 import {
   Typography,
   Box,
+  AppBar,
   Avatar,
+  Toolbar,
   FormControl,
   InputLabel,
   Input,
@@ -11,6 +13,7 @@ import {
   Button,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import Logo from "../assets/images/CoBuildLogo.png";
 
 const useStyles = makeStyles({
   root: {
@@ -42,86 +45,197 @@ const useStyles = makeStyles({
   },
 });
 
-const Step3 = ({ handleSetProfileImage }) => {
+const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
   const classes = useStyles();
-  const [profileImage, setProfileImage] = useState(null);
   const navigate = useNavigate();
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
-  const handleProfileImageSubmit = async (event) => {
-    if (!event.target || !event.target.files || event.target.files.length <= 0) return;
-    const file = event.target.files[0];
-    if (!file) return;
-    const uname = "user1";
-    const type = "profile";
-    const extension = "jpg";
-    const {url} = await fetch(`http://localhost:8000/s3Url?username=${uname}&type=${type}&extension=${extension}`).then(res => res.json());
-    await fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      body: file
-    });
-    navigate("/users");
-  };
-
-  const handleProfileImageChange = async (event) => {
-    if (!event.target || !event.target.files || event.target.files.length <= 0) return;
-    const file = event.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setProfileImage(reader.result);
-      };
-      reader.readAsDataURL(file);
-      
-    }
-  };
-
-  
   const handlePreviewClick = () => {
     // Implement any custom logic when the profile image preview is clicked
+  };
+
+  const handlePreviousClick = () => {
+    handlePrevious(); // Trigger the navigation to Step1
   };
 
   return (
     <div className={classes.root}>
       <Typography variant="h4" gutterBottom>
-        Profile Photo
+        Upload Files
       </Typography>
 
-      <Box className={classes.uploadContainer}>
-        <FormControl>
-          {/* <InputLabel htmlFor="profile-image-input">Upload Image</InputLabel> */}
-          <Input
-            id="profile-image-input"
-            type="file"
-            accept="image/*"
-            onChange={handleProfileImageChange}
-          />
-          <FormHelperText>Accepted formats: JPG, PNG</FormHelperText>
-        </FormControl>
-      </Box>
+      <h3>Please upload a resume, cover letter, or transcript</h3>
 
-      {profileImage && (
-        <Box className={classes.previewContainer}>
-          <Avatar
-            className={classes.avatar}
-            alt="Profile Image"
-            src={profileImage}
-            onClick={handlePreviewClick}
-          />
-          <Typography variant="subtitle1">Preview</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={() => handleProfileImageSubmit(profileImage)}
-          >
-            Set Profile Picture
+          <label
+                  htmlFor="fileInput"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <span
+                    style={{
+                      boxSizing: "border-box",
+                      width: "330px",
+                      height: "160px",
+                      background: "#F3F0FF",
+                      border: "2px dashed #7A5FEC", // Adjust border color and thickness
+                      borderRadius: "8px",
+                      transition: "transform 0.3s ease",
+                      transform: "scale(1)",
+                      /* Inside auto layout */
+                      flex: "none",
+                      order: "0",
+                      alignSelf: "stretch",
+                      flexGrow: "0",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      color: "#7A5FEC",
+                      WebkitTextStrokeWidth: "1px",
+                      fontWeight: "normal",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.animation = "circle 1s infinite linear";
+                      e.target.style.transform = "scale(1.05)"; // Increase the scale on hover
+                    }}
+                    onMouseLeave={(e) => {
+                      e.target.style.animation = "none";
+                      e.target.style.transform = "scale(1)"; // Reset the scale when not hovered
+                    }}
+                  >
+                    Import Files
+                  </span>
+                  <input
+                    type="file"
+                    id="fileInput"
+                    accept="application/pdf"
+                    style={{ display: "none" }}
+                    required
+                    capture="user"
+                    onChange={(e) => {
+                      const files = e.target.files;
+                      const updatedFiles = [...uploadedFiles];
+
+                      for (let i = 0; i < files.length; i++) {
+                        updatedFiles.push(files[i]);
+                      }
+
+                      setUploadedFiles(updatedFiles);
+                    }}
+                  />
+                </label>
+                {uploadedFiles.map((file, index) => (
+                  // <div key={index}>
+                  //   <span>{file.name}</span>
+                  // </div>
+
+                  <div
+                    key={index}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      padding: "0px",
+                      gap: "12px",
+                      width: "330px",
+                      height: "160px", // Adjusted height to match the previous div length
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        padding: "8px",
+                        gap: "12px",
+                        width: "330px",
+                        height: "72px",
+                        background: "#FFFFFF",
+                        boxShadow: "4px #EAE2FD",
+                        borderRadius: "8px",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "48px",
+                          height: "56px",
+                          background: "#DAF2D9",
+                          borderRadius: "4px",
+                          flex: "none",
+                          order: "0",
+                          flexGrow: "0",
+                        }}
+                      ></div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          alignItems: "flex-start",
+                          padding: "0px",
+                          gap: "4px",
+                          width: "254px",
+                          height: "56px",
+                          flex: "none",
+                          order: "1",
+                          flexGrow: "1",
+                        }}
+                      >
+                        <p
+                          style={{
+                            width: "254px", // Adjusted width to match the previous div width
+                            height: "18px",
+                            fontFamily: "Inter",
+                            fontStyle: "normal",
+                            fontWeight: 700,
+                            fontSize: "14px",
+                            lineHeight: "130%",
+                            display: "flex",
+                            alignItems: "center",
+                            color: "#575361",
+                            overflow: "hidden",
+                            whiteSpace: "nowrap",
+                            textOverflow: "ellipsis",
+                          }}
+                          title={file.name} // Add the title attribute to display the full name on hover
+                        >
+                          {file.name}
+                        </p>
+                        <p
+                          style={{
+                            width: "254px", // Adjusted width to match the previous div width
+                            height: "16px",
+                            fontFamily: "'Inter'",
+                            fontStyle: "normal",
+                            fontWeight: "500",
+                            fontSize: "12px",
+                            lineHeight: "130%",
+                            display: "flex",
+                            alignItems: "center",
+                            color: "#857E95",
+                            flex: "none",
+                            order: "1",
+                            alignSelf: "stretch",
+                            flexGrow: "0",
+                          }}
+                        >
+                          {Math.round(file.size / 1024)} KB
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+        <Box marginTop={2}>
+          <Button variant="outlined" onClick={handlePrevious}>
+            Previous
+          </Button>
+          <Button variant="contained" color="primary" onClick={handleNext}>
+            Continue
           </Button>
         </Box>
-      )}
     </div>
   );
 };
