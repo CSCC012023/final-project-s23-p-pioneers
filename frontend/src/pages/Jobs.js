30575705e15158fbc44bdf387b8527d1cb0bc924
Dhruvin Patel <104400084+pdhruvin25@users.noreply.github.com>
2023-07-17 19:42:01 -0400

    COB-6: Added the functionality to filter based on location and title. Moreover, changed the UI of the overall cards, to be more visually appealing

--- frontend/src/pages/Jobs.js
+++ frontend/src/pages/Jobs.js
@@ -1,13 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import JobPosting from "./components/Card";
+import { useNavigate } from "react-router-dom";
+
+import {
+  AppBar,
+  Avatar,
+  Typography,
+  Button,
+  Grid,
+  TextField,
+  BottomNavigation,
+  Toolbar,
+  InputAdornment,
+} from "@mui/material";
+import Logo from "../assets/images/CoBuildLogo.png";
 
 function Jobs() {
   const [jobs, setJobs] = useState([]);
+  const [location, setLocation] = useState("");
+  const [search, setSearch] = useState("");
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [shouldFilter, setShouldFilter] = useState(false);
+
+  const handleLocationChange = (event) => {
+    const { value } = event.target;
+    setLocation(value);
+  };
+
+  const handleSearchChange = (event) => {
+    const { value } = event.target;
+    setSearch(value);
+  };
 
   useEffect(() => {
     fetchJobIds();
   }, []);
+
+  useEffect(() => {
+    if (shouldFilter) {
+      filterJobs();
+    }
+  }, [shouldFilter]);
 
   const fetchJobIds = () => {
     console.log("hello");
@@ -22,16 +56,229 @@ function Jobs() {
       .then((data) => {
         console.log(data);
         setJobs(data);
+        setFilteredJobs(data);
       })
       .catch((error) => {
         console.error("Error fetching job posts:", error);
       });
   };
 
+  const filterJobs = () => {
+    const filtered = jobs.filter((job) => {
+      const titleMatch = search ? job.title.toLowerCase().includes(search.toLowerCase()) : true;
+      const locationMatch = location ? job.location.toLowerCase().includes(location.toLowerCase()) : true;
+      return titleMatch && locationMatch;
+    });
+    setFilteredJobs(filtered);
+    setShouldFilter(false);
+  };
+
+  const handleGoButtonClick = () => {
+    setShouldFilter(true);
+  };
+
   return (
-    <div style={{ marginBottom: "50px" }}>
+    <div style={{ backgroundColor: "#3B3B3B" }}>
       {/* Render job list or perform actions with jobIds */}
-      <JobPosting prop={jobs} />
+      <AppBar position="relative" style={{ background: "#2B2B2B", height: "80px" }}>
+        <Toolbar
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "80px",
+            justifyContent: "space-between",
+          }}
+        >
+          <Avatar alt="Logo" src={Logo} style={{ width: "35px", height: "35px" }} />
+          <Typography
+            variant="h6"
+            style={{
+              fontWeight: "bold",
+              marginLeft: "25px",
+              fontSize: "30px",
+              fontFamily: "work sans",
+            }}
+          >
+            CoBuild
+          </Typography>
+
+          <Button
+            component={Link}
+            to="/login"
+            style={{
+              background: "#A259FF",
+              marginLeft: "auto",
+              width: "120px",
+              height: "60px",
+              borderRadius: "20px",
+              fontWeight: 600,
+              color: "#FFFFFF",
+              fontStyle: "normal",
+              fontSize: "16px",
+              lineHeight: "140%",
+              fontFamily: "work sans",
+            }}
+          >
+            Sign In
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div style={{ paddingTop: "20px", backgroundColor: "#2B2B2B" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            padding: "0px",
+            gap: "30px",
+            width: "1050px",
+
+            margin: "0 auto",
+          }}
+        >
+          <Typography
+            variant="h4"
+            style={{
+              float: "left",
+              fontFamily: "work sans",
+              fontWeight: "bold",
+              fontSize: "45px",
+            }}
+          >
+            Browse Job Postings
+          </Typography>
+          <Typography
+            stlye={{ float: "left", fontFamily: "work sans", fontSize: "25px" }}
+          >
+            Browse through more than 100s of job postings on CoBuild Job Board
+          </Typography>
+
+          <TextField
+            fullWidth
+            label="Location"
+            variant="outlined"
+            value={location}
+            onChange={handleLocationChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#FFFFFF", // White border color
+                  "&:hover": {
+                    borderColor: "#A259FF !important", // Purple border color on hover
+                  },
+                },
+                "& input": {
+                  color: "#FFFFFF", // White text color
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: "#FFFFFF", // White text color for the label
+              },
+            }}
+          >
+            Location
+          </TextField>
+          <TextField
+            fullWidth
+            label="Search For Jobs"
+            variant="outlined"
+            value={search}
+            onChange={handleSearchChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#FFFFFF", // White border color
+                  "&:hover": {
+                    borderColor: "#A259FF !important", // Purple border color on hover
+                  },
+                },
+                "& input": {
+                  color: "#FFFFFF", // White text color
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: "#FFFFFF", // White text color for the label
+              },
+            }}
+          >
+            Search For Jobs
+          </TextField>
+          <Button
+            variant="contained"
+            onClick={handleGoButtonClick}
+            sx={{
+              backgroundColor: "#A259FF",
+              fontFamily: "work sans",
+              fontWeight: 600,
+              color: "#FFFFFF",
+              fontStyle: "normal",
+              fontSize: "16px",
+              lineHeight: "140%",
+              transition: "background-color 0.3s",
+              "&:hover": {
+                backgroundColor: "#FFFFFF",
+                color: "#A259FF",
+              },
+            }}
+          >
+            Filter
+          </Button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+          alignSelf: "stretch",
+          backgroundColor: "#2B2B2B",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+          padding: "20px", // Increase the padding value to add more space at the bottom
+        }}
+      >
+        <Grid container spacing={0} justifyContent="center" alignItems="center">
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{
+                backgroundColor: "#3B3B3B",
+                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+                transition: "background-color 0.3s",
+                height: "calc(100% - 10px)",
+                fontFamily: "work sans",
+              }}
+              onMouseEnter={(e) => (e.target.style.backgroundColor = "#A259FF")}
+              onMouseLeave={(e) => (e.target.style.backgroundColor = "#3B3B3B")}
+            >
+              Jobs
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{
+                backgroundColor: "#3B3B3B",
+                boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.25)",
+                transition: "background-color 0.3s",
+                height: "calc(100% - 10px)",
+                fontFamily: "work sans",
+              }}
+              onMouseEnter={(e) => (e.target.style.backgroundColor = "#A259FF")}
+              onMouseLeave={(e) => (e.target.style.backgroundColor = "#3B3B3B")}
+            >
+              Assessments
+            </Button>
+          </Grid>
+        </Grid>
+
+      </div>
+      <JobPosting prop={filteredJobs} />
+
     </div>
   );
 }
--- frontend/src/pages/components/Card.js
+++ frontend/src/pages/components/Card.js
@@ -3,6 +3,7 @@ import { Container, Grid, Box, Typography } from "@mui/material";
 import "../JobPosting.css"; // Import custom CSS file for styling
 import { useNavigate } from "react-router-dom";
 
+
 function JobPosting(prop) {
   const navigate = useNavigate();
 
@@ -38,50 +39,97 @@ const JobGrid = ({ jobs }) => {
             >
               <Box
                 sx={{
-                  padding: 2,
-                  border: "1px solid #e0e0e0",
-                  borderRadius: 4,
-                  transition: "box-shadow 0.3s",
-                  "&:hover": {
-                    boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.2)",
-                  },
+                  padding: 3,
+    border: "1px solid rgba(224, 224, 224, 0)",
+    borderRadius: 4,
+    transition: "box-shadow 0.3s ease",
+    "&:hover": {
+      boxShadow:
+        "0px 4px 12px rgba(0, 0, 0, 0.2), 0px 0px 20px 5px rgba(219, 203, 178, 0.5)",
+    },
+    boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.1)",
+    width: "100%",
+    backgroundColor: "#2B2B2B"
                 }}
               >
-                <img
-                  src={
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
-                  }
-                  alt={job.title}
-                  style={{
-                    width: "100%",
-                    height: "295px", // Adjust as needed
-                    objectFit: "cover",
-                  }}
-                />
+                <div style={{ position: "relative" }}>
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
+                    alt={job.title}
+                    style={{
+                      width: "100%",
+                      height: "50%", // Adjusted height to match the top half of the box
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
 
-                <Typography
-                  variant="subtitle1"
-                  sx={{
-                    color: "white",
-                    fontFamily: "Work Sans",
-                    fontStyle: "Sans Serif",
-                    fontWeight: 600,
-                    fontSize: "22px",
-                    lineHeight: "140%",
-                  }}
-                >
-                  {job.title}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  sx={{
-                    fontFamily: "Work Sans, sans-serif",
-                    color: "white",
-                  }}
-                >
-                  {`${job.companyName} • ${job.location} • ${job.datePosted}`}
-                </Typography>
+                <div
+  style={{
+    display: "flex",
+    flexWrap: "wrap", // Added flex-wrap property
+    padding: "10px 5px 5px 5px",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: "5px", // Adjusted gap value
+    alignSelf: "stretch",
+  }}
+>
+  <Typography
+    variant="subtitle1"
+    sx={{
+      color: "white",
+      fontFamily: "Work Sans",
+      fontWeight: "bold",
+      fontSize: "30px",
+      lineHeight: "140%",
+
+    }}
+  >
+    {job.title}
+  </Typography>
+  <Typography
+    variant="Company"
+    color="textSecondary"
+    sx={{
+      fontFamily: "Work Sans, sans-serif",
+      color: "white",
+      fontSize: "20px",
+    }}
+  >
+    {job.companyName}
+  </Typography>
+  <Typography
+    variant="caption"
+    color="textSecondary"
+    sx={{
+      fontFamily: "Work Sans, sans-serif",
+      color: "white",
+      fontSize: "15px",
+      fontWeight: "bold",
+    }}
+  >
+    <span style={{ color: "#7A5FEC", fontSize: "11px" }}>LOCATION</span>
+    <br /> {job.location} {/* Added line break */}
+  </Typography>
+  <Typography
+    variant="caption"
+    color="textSecondary"
+    sx={{
+      fontFamily: "Work Sans, sans-serif",
+      color: "white",
+      fontSize: "15px",
+      fontWeight: "bold",
+
+    }}
+  >
+    <span style={{ color: "#7A5FEC", fontSize: "11px" }}>DATE</span>
+    <br /> {job.datePosted} {/* Added line break */}
+  </Typography>
+</div>
+
+
+
               </Box>
             </Grid>
           ))
