768ddd0690818b7f339704562832445b70ab11c0
Vikram Narra <vchandra.narra@gmail.com>
2023-07-25 18:14:24 -0400

    <fix>:Bug Fixed

--- frontend/src/pages/Step3.js
+++ frontend/src/pages/Step3.js
@@ -151,7 +151,7 @@ const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <label
-                  htmlFor="fileInput"
+                  htmlFor="resumeInput"
                   style={{
                     display: "flex",
                     justifyContent: "center",
@@ -189,11 +189,172 @@ const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
                       e.target.style.transform = "scale(1)"; // Reset the scale when not hovered
                     }}
                   >
-                    Import Files
+                    Import Resume
                   </span>
                   <input
                     type="file"
-                    id="fileInput"
+                    id="resumeInput"
+                    accept="application/pdf"
+                    style={{ display: "none" }}
+                    required
+                    capture="user"
+                    onChange={(e) => {
+                      const files = e.target.files;
+                      const updatedFiles = [...uploadedFiles];
+
+                      for (let i = 0; i < files.length; i++) {
+                        updatedFiles.push(files[i]);
+                      }
+                      setUploadedResume(updatedFiles);
+                    }}
+                  />
+                </label>
+                {uploadedResume.map((file, index) => (
+                  // <div key={index}>
+                  //   <span>{file.name}</span>
+                  // </div>
+
+                  <div
+                    key={index}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      paddingTop: "8px",
+                      gap: "12px",
+                      width: (window.innerWidth/5),
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        padding: "8px",
+                        gap: "12px",
+                        width: (window.innerWidth/5),
+                        height: "72px",
+                        background: "#FFFFFF",
+                        boxShadow: "4px #EAE2FD",
+                        borderRadius: "8px",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "48px",
+                          height: "56px",
+                          background: "#DAF2D9",
+                          borderRadius: "4px",
+                          flex: "none",
+                          order: "0",
+                          flexGrow: "0",
+                        }}
+                      ></div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          alignItems: "flex-start",
+                          padding: "0px",
+                          gap: "4px",
+                          width: (window.innerWidth/5),
+                          height: "56px",
+                          flex: "none",
+                          order: "1",
+                          flexGrow: "1",
+                        }}
+                      >
+                        <p
+                          style={{
+                            width: (window.innerWidth/5), // Adjusted width to match the previous div width
+                            height: "18px",
+                            fontFamily: "Inter",
+                            fontStyle: "normal",
+                            fontWeight: 700,
+                            fontSize: "14px",
+                            lineHeight: "130%",
+                            display: "flex",
+                            alignItems: "center",
+                            color: "#575361",
+                            overflow: "hidden",
+                            whiteSpace: "nowrap",
+                            textOverflow: "ellipsis",
+                          }}
+                          title={file.name} // Add the title attribute to display the full name on hover
+                        >
+                          {file.name}
+                        </p>
+                        <p
+                          style={{
+                            width: (window.innerWidth/5), // Adjusted width to match the previous div width
+                            height: "16px",
+                            fontFamily: "'Inter'",
+                            fontStyle: "normal",
+                            fontWeight: "500",
+                            fontSize: "12px",
+                            lineHeight: "130%",
+                            display: "flex",
+                            alignItems: "center",
+                            color: "#857E95",
+                            flex: "none",
+                            order: "1",
+                            alignSelf: "stretch",
+                            flexGrow: "0",
+                          }}
+                        >
+                          {Math.round(file.size / 1024)} KB
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+        </Grid>
+        <Grid item xs={12} md={6}>
+        <label
+                  htmlFor="transcriptInput"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <span
+                    style={{
+                      boxSizing: "border-box",
+                      width: (window.innerWidth/5),
+                      height: "160px",
+                      background: "#F3F0FF",
+                      border: "2px dashed #7A5FEC", // Adjust border color and thickness
+                      borderRadius: "8px",
+                      transition: "transform 0.3s ease",
+                      transform: "scale(1)",
+                      /* Inside auto layout */
+                      flex: "none",
+                      order: "0",
+                      alignSelf: "stretch",
+                      flexGrow: "0",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      color: "#7A5FEC",
+                      WebkitTextStrokeWidth: "1px",
+                      fontWeight: "normal",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.animation = "circle 1s infinite linear";
+                      e.target.style.transform = "scale(1.05)"; // Increase the scale on hover
+                    }}
+                    onMouseLeave={(e) => {
+                      e.target.style.animation = "none";
+                      e.target.style.transform = "scale(1)"; // Reset the scale when not hovered
+                    }}
+                  >
+                    Import Transcript
+                  </span>
+                  <input
+                    type="file"
+                    id="transcriptInput"
                     accept="application/pdf"
                     style={{ display: "none" }}
                     required
@@ -206,11 +367,11 @@ const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
                         updatedFiles.push(files[i]);
                       }
 
-                      setUploadedFiles(updatedFiles);
+                      setUploadedTranscript(updatedFiles);
                     }}
                   />
                 </label>
-                {uploadedFiles.map((file, index) => (
+                {uploadedTranscript.map((file, index) => (
                   // <div key={index}>
                   //   <span>{file.name}</span>
                   // </div>
@@ -221,7 +382,7 @@ const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
                       display: "flex",
                       flexDirection: "column",
                       alignItems: "flex-start",
-                      padding: "0px",
+                      paddingTop: "8px",
                       gap: "12px",
                       width: (window.innerWidth/5),
                     }}
@@ -310,9 +471,24 @@ const Step3 = ({ handleSetProfileImage, handleNext, handlePrevious }) => {
                     </div>
                   </div>
                 ))}
+        </Grid>
+      </Grid>
 
-        <Box marginTop={2}>
-          <Button variant="outlined" onClick={handlePrevious}>
+      
+
+      <Grid container spacing={2} paddingTop={2}>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={handleNextClick}
+          >
+            Continue
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="outlined" color="secondary" fullWidth onClick={handlePrevious}>
             Previous
           </Button>
         </Grid>
